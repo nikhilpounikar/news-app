42493aaf7da6a4d9875b03d6f32862c5b5e74db6
nikhilpounikar <nikhilpounikar@gmail.com>
2024-02-01 10:52:07 +0530

    first commit

---
+++ product-service-maven/product-service/src/test/java/com/project/microservice/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.microservice.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
