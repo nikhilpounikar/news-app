e257f17720b273b688084369c8f5235efa870a76
nikhilpounikar <nikhilpounikar@gmail.com>
2024-02-26 14:03:50 +0530

    social interaction service created

---
+++ SocialInteractionService/src/main/java/com/application/SocialInteractionService/SocialInteractionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.application.SocialInteractionService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialInteractionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialInteractionServiceApplication.class, args);
+	}
+
+}
